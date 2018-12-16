3dba8fdf8485beef2e3e4e9bc2d6cbb87e82c5e1
Aasbin Chhetri <aasbin.chhetri@ewah.charmlab.bootstack>
2018-12-16 12:36:13 +0000

    first draft ready for get-capacity-info action

--- actions/get_capacity_info.py
+++ actions/get_capacity_info.py
@@ -2,8 +2,9 @@
 import os
 import sys
 from novaclient import client
-from keystoneauth1 import session
-
+from novaclient import exceptions as novaexceptions
+from keystoneauth1 import exceptions as keystoneexceptions
+import yaml
 _path = os.path.dirname(os.path.realpath(__file__))
 _root = os.path.abspath(os.path.join(_path, '..'))
 
@@ -14,111 +15,107 @@ def _add_path(path):
 
 _add_path(_root)
 
-#sys.path.append('../')
 import charmhelpers.core.hookenv as hookenv
-#import hooks.nova_cc_utils as ncc_utils
 import charmhelpers.contrib.openstack.keystone as keystone
 from charmhelpers.fetch import apt_install
-from charmhelpers.core.hookenv import (
-    log,
-    ERROR,
-)
 from charmhelpers.contrib.openstack.context import IdentityServiceContext
 config =  hookenv.config()
 
 ram_allocation_ratio = config['ram-allocation-ratio']
 cpu_allocation_ratio = config['cpu-allocation-ratio']
 
-av = 0
 
 
 
 
 def can_host_flavor(hypervisor, flavor, ram_allocation_ratio, cpu_allocation_ratio):
-    count = 0
-    free_mem = hypervisor.memory_mb * ram_allocation_ratio - hypervisor.memory_mb_used
-    free_vcpu = hypervisor.vcpus * cpu_allocation_ratio - hypervisor.vcpus_used
-    while True:
-        if free_mem >= flavor.ram and free_vcpu >= flavor.vcpus:
-            count += 1
-            free_mem -= flavor.ram
-            free_vcpu -= flavor.vcpus
-        else:
-            return count
+              count = 0
+              free_mem = hypervisor.free_ram_mb * ram_allocation_ratio
+              free_vcpu = hypervisor.vcpus * cpu_allocation_ratio - hypervisor.vcpus_used
+              while True:
+                  if free_mem >= flavor.ram and free_vcpu >= flavor.vcpus:
+                      count += 1
+                      free_mem -= flavor.ram
+                      free_vcpu -= flavor.vcpus
+                  else:
+                      return count
 
-def get_apiversion_and_endpoint():
-    context = IdentityServiceContext()()
-    if not context:
-        msg = "Identity service context cannot be generated"
-        log(msg, level=ERROR)
-        raise ValueError(msg)
+def get_novaapi():
+              context = IdentityServiceContext()()
+              try:
+                  if int(context['api_version']) == 2:
+                    api_version = 2
+                    endpoint = '%s://%s:%s/v%s' % (context['service_protocol'],
+                                             context['service_host'],
+                                             context['service_port'],
+                                             float(context['api_version']))
+                  else:
+                    api_version = 3
+                    endpoint = '%s://%s:%s/v%s' % (context['service_protocol'],
+                                             context['service_host'],
+                                             context['service_port'],
+                                             context['api_version'])
+                  novaapi = client.Client('2.2', session= get_auth_session(endpoint, api_version, context))
+                  return novaapi
+              except  Exception, e:
+                  hookenv.action_fail(e.message)
+                  hookenv.action_set({"error": "Did not receive the openstack-auth parameters from identity context"})
+                  sys.exit(1)
+                  return "unknown"
+def get_auth_session(endpoint, api_version, context):
+            if api_version == 2:
+              try:
+                    from keystoneclient.auth.identity import v2
+                    from keystoneauth1 import session
+              except ImportError:
+                    if six.PY2:
+                        apt_install(["python-keystoneclient"], fatal=True)
+                    else:
+                        apt_install(["python3-keystoneclient"], fatal=True)
+                    from keystoneclient.auth.identity import v2
+                    from keystoneauth1 import session
+              auth = v2.Password(auth_url=endpoint, username=context['admin_user'], password=context['admin_password'], tenant_name=context['admin_tenant_name'])
+            if api_version == 3:
+              try:
+                    from keystoneclient import session
+                    from keystoneclient.auth.identity import v3
+              except ImportError:
+                    if six.PY2:
+                        apt_install(["python-keystoneclient"], fatal=True)
+                    else:
+                        apt_install(["python3-keystoneclient"], fatal=True)
 
-
-    if int(context['api_version']) == 2:
-      api_version = 2
-      endpoint = '%s://%s:%s/v%s' % (context['service_protocol'],
-                               context['service_host'],
-                               context['service_port'],
-                               float(context['api_version']))
-    else:
-      api_version = 3
-      endpoint = '%s://%s:%s/v%s' % (context['service_protocol'],
-                               context['service_host'],
-                               context['service_port'],
-                               context['api_version'])
-    novaapi = client.Client('2.2', session=get_keystone_session(endpoint, api_version, context))
-    return novaapi
-def get_keystone_session(endpoint, api_version, context):
-    av = context['api_version']
-    hookenv.action_set({'get-capacity-info': endpoint})
-    if api_version == 2:
-      try:
-            from keystoneclient.auth.identity import v2
-            from keystoneclient import session
-      except ImportError:
-            if six.PY2:
-                apt_install(["python-keystoneclient"], fatal=True)
-            else:
-                apt_install(["python3-keystoneclient"], fatal=True)
-            from keystoneclient.auth.identity import v2
-            from keystoneclient import session
-      auth = v2.Password(auth_url=endpoint, username=context['admin_user'], password=context['admin_password'], tenant_name=context['admin_tenant_name'])
-    if api_version == 3:
-      try:
-            from keystoneclient import session
-            from keystoneclient.auth.identity import v3
-      except ImportError:
-            if six.PY2:
-                apt_install(["python-keystoneclient"], fatal=True)
-            else:
-                apt_install(["python3-keystoneclient"], fatal=True)
-
-            from keystoneclient.auth import token_endpoint
-            from keystoneclient import session
-            from keystoneclient.auth.identity import v3
-      auth = v3.Password(project_name=context['admin_tenant_name'], project_domain_name='Default', auth_url=endpoint, username=context['admin_user'], password=context['admin_password'], user_domain_name=context['admin_domain_id'])
-    return session.Session(auth=auth)
+                    from keystoneclient.auth import token_endpoint
+                    from keystoneauth1 import session
+                    from keystoneclient.auth.identity import v3
+              auth = v3.Password(auth_url=endpoint, username=context['admin_user'], password=context['admin_password'], user_domain_id=context['admin_domain_id'])
+            return session.Session(auth=auth)
 
 
 def main():
-   nova = get_apiversion_and_endpoint()
-   capacityoutput = []
-   flavorcapacityoutput = []
-   hypervisors = nova.hypervisors.list()
-   flavors = nova.flavors.list()
-   s = ''
-   for flavor in flavors:
-     totalinstances_per_flavor = 0
-     for hypervisor in hypervisors:
-       totalinstances =  can_host_flavor(hypervisor, flavor, ram_allocation_ratio, cpu_allocation_ratio)
-       totalinstances_per_flavor += totalinstances
-       if not  hypervisor.hypervisor_hostname in capacityoutput:
-         capacityoutput += [ hypervisor.hypervisor_hostname,' ']
-       capacityoutput.append('{}:  {} '.format(flavor.name, totalinstances))
+           nova = get_novaapi()
+           try:
+              hypervisors = nova.hypervisors.list()
+              flavors = nova.flavors.list()
+           except Exception, e:
+              msg = str(e)
+              hookenv.action_fail(msg)
+              hookenv.action_set({"error": " error from api {} ".format(msg)})
+              sys.exit(1)
+           flavor_instances = {}
+           totalcapacity = {}
+           output = {}
+           for flavor in flavors:
+             totalinstances_per_flavor = 0
+             flavor_name = str(flavor.name)
+             for hypervisor in hypervisors:
+               hostname = str(hypervisor.hypervisor_hostname)
+               totalinstances =  can_host_flavor(hypervisor, flavor, ram_allocation_ratio, cpu_allocation_ratio)
+               totalinstances_per_flavor += totalinstances
+               flavor_instances.update({ flavor_name : totalinstances, })
+               output.update({ hostname : { flavor_name : totalinstances, } }) if hostname not in output else output[hostname].update({ flavor_name : totalinstances, })
+             totalcapacity.update( {str(flavor.name) : totalinstances_per_flavor, } )
+             output.update({"Total Capacity" : totalcapacity, } ) 
+           hookenv.action_set({'get-capacity-info': yaml.dump(output)})
 
-     flavorcapacityoutput.append('Total_capacity flavor {}:  {} \n'.format(flavor.name,totalinstances_per_flavor))
-   flavorcapacityoutput += capacityoutput
-   print(s.join(flavorcapacityoutput))
-   hookenv.action_set({'get-capacity-info': s.join(flavorcapacityoutput)})
 main()
-
